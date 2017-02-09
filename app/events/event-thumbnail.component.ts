61f51ee3905ce6a51b1f3793f118be0365b348fb
Adrian Jimenez <adrianx30@gmail.com>
2017-02-09 14:20:29 -0500

    Get data from own service

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,19 +4,30 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
     selector: 'event-thumbnail',
     template: `
     <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-            <span>Location: {{event.location.address}}</span>
-            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+            Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Lately Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
+        </div>
+        <div>Price: \${{event?.price}}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            <span class="pad-left">{{event?.location?.city}}, 
+            {{event?.location?.country}}</span>
+        </div>
+        <div *ngIf="event?.onlineUrl"> 
+        OnlineUrl: {{event?.onlineUrl}}
         </div>
         <!-- <button class="btn btn-primary" (click)="handleClickMe()
             ">Click me! </button> -->
     </div>
     `,
     styles: [`
+    .green {color: #003300 !important;}
+    .bold {font-weight: bold;}
     .pad-left {margin-left: 10px;}
     .well div {color: #bbb;}
     .thumbnail {min-height: 210px;}
@@ -24,6 +35,25 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
 })
 export class EventThumbnailComponent {
     @Input() event: any
+
+    getStartTimeClass(){
+        const isEarlyStart = this.event && this.event.time === '8:00 am'
+        return {green: isEarlyStart, bold: isEarlyStart}
+        /*
+        -- Another option to return
+        if(this.event && this.event.time === '8:00 am')
+            return 'green bold' --or -- return ['green', 'bold']
+        return '' --or -- return []
+        */
+    }
+
+    //Used as getStartTimeClass() but use [ngStyle] instead [ngClass]
+    getStartTimeStyle():any{
+        if(this.event && this.event.time === '8:00 am')
+            return {color: '#003300', 'font-weight': 'bold'}
+        return {}
+    }
+
 
     /* @Output() eventClick = new EventEmitter()
 
